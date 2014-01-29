3cb6b33ad8ef2e5a06f3a238decf68d2d58653cd
wells <wells@spotify.com>
2014-01-29 16:14:16 -0500

    Cleaned up class object

--- visualizations/milkshake.js
+++ visualizations/milkshake.js
@@ -1,5 +1,4 @@
 var milk = (function(){
-
     // var req = new XMLHttpRequest();
     // req.open("GET", "/milkshake/Class.js", false); req.send(); eval(req.responseText);
     /* Simple JavaScript Inheritance
@@ -7,38 +6,40 @@ var milk = (function(){
      * MIT Licensed.
      */
     // Inspired by base2 and Prototype
-    (function(){
-        var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-        this.Class = function(){};
-        Class.extend = function(prop) {
-    	var _super = this.prototype;
-    	initializing = true;
-    	var prototype = new this();
-    	initializing = false;
-    	for (var name in prop) {
-    	    prototype[name] = typeof prop[name] == "function" &&
-    		typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-    		(function(name, fn){
-    		    return function() {
-    			var tmp = this._super;
-    			this._super = _super[name];
-    			var ret = fn.apply(this, arguments);
-    			this._super = tmp;
-    			return ret;
-    		    };
-    		})(name, prop[name]) :
-    		prop[name];
-    	}
-    	function Class() {
-    	    if ( !initializing && this.init )
-    		this.init.apply(this, arguments);
-    	}
-    	Class.prototype = prototype;
-    	Class.prototype.constructor = Class;
-    	Class.extend = arguments.callee;
-    	return Class;
-        };
-    })();
+  var Class =  (function(){
+    var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+    var _Class = function(){};
+    _Class.extend = function(prop) {
+      var _super = this.prototype;
+      initializing = true;
+      var prototype = new this();
+      initializing = false;
+      for (var name in prop) {
+        prototype[name] = typeof prop[name] == "function" &&
+          typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+          (function(name, fn){
+          return function() {
+            var tmp = this._super;
+            this._super = _super[name];
+            var ret = fn.apply(this, arguments);
+            this._super = tmp;
+            return ret;
+          };
+        })(name, prop[name]) :
+          prop[name];
+      }
+      function Class() {
+        if ( !initializing && this.init )
+          this.init.apply(this, arguments);
+      }
+      Class.prototype = prototype;
+      Class.prototype.constructor = Class;
+      Class.extend = arguments.callee;
+      return Class;
+    };
+    return _Class;
+  })();
+
     // req.open("GET", "/milkshake/Shaker.js", false); req.send(); eval(req.responseText);
     var Shaker = Class.extend({
 
